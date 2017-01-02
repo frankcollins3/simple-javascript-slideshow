df6fb88e6e8008eb954b1e83088dd95ae8d71a22
Tim Erickson <tim@politalk.org>
2017-01-02 15:55:52 -0600

    Cleaning up the code a little

--- java.js
+++ java.js
@@ -1,24 +1,49 @@
-function showSlides() {
-  console.log("Show Slides");
+/* 
+My initial approach for this project is to totally 
+manage the images in Javascript. User can enter image file 
+names into opening script of HTML doc. 
+
+At any given time, Javascript is passing along three images to the DOM. 
+The "current", "previous," and "next" image. Each time the slider moves
+the array is updated as well as the images that are being 
+passed along to the DOM.
+*/
+
+updateSlides();
+document.getElementById("frame").style.marginLeft = "-600px";
+//console.log("Start " + slides[1]);
+
+var clickBack = document.querySelector('.back');
+var clickForward = document.querySelector('.forward');
+
+clickBack.addEventListener('click', moveBack);
+clickForward.addEventListener('click', moveForward);
+
+/* upDate slides is the function that updates the DOM with the three 
+active images at any given time. */
+
+function updateSlides() {
+  document.getElementById('frame').innerHTML =  
+  '<div><img src="images/' + slides[2] + '"></div><div><img src="images/' + slides[1] + '"></div><div><img src="images/' + slides[0] + '"></div>';
 }
 
 
-document.getElementById('frame').innerHTML =  
-  '<div><img src="images/' + slides[2] + '"></div><div><img src="images/' + slides[1] + '"></div><div><img src="images/' + slides[0] + '"></div>';
-document.getElementById("frame").style.marginLeft = "-600px";
-console.log("Start " + slides[1]);
+/* Moves images in array backward One */
+function updateArrayBack() {
+  var temp = slides.pop();
+  slides.unshift(temp);
+}
 
-var leftClick = document.querySelector('.back');
-var rightClick = document.querySelector('.forward');
+/* Moves images in array forward One */
+function updateArrayForward () {
+  var temp = slides.shift();
+  slides.push(temp);
+}
 
-leftClick.addEventListener('click', moveBack);
-rightClick.addEventListener('click', moveForward);
-
+/* Animates the slideshow backwards */
 function moveBack() {
-  document.getElementById('frame').innerHTML =  
-  '<div><img src="images/' + slides[2] + '"></div><div><img src="images/' + slides[1] + '"></div><div><img src="images/' + slides[0] + '"></div>';
-
-  console.log("Start Backwards " + slides[1]);
+  updateSlides();
+  //console.log("Start Backwards " + slides[1]);
   var id = setInterval(frame, 1);
   var left = -600;
   function frame() {
@@ -29,13 +54,13 @@ function moveBack() {
       document.getElementById("frame").style.marginLeft = (left) + "px";
     }
   };
-  var temp = slides.pop();
-  slides.unshift(temp);
+  updateArrayBack() 
   document.getElementById("frame").style.marginLeft = "-600px";
-  console.log("Finish Backwards" + slides[1]);
-  console.log(slides);
+  //console.log("Finish Backwards" + slides[1]);
+  //console.log(slides);
 }
 
+/* Animates the slideshow forwards */
 function moveForward () {
   console.log("Start Forward " + slides[1]);
   var id = setInterval(frame, 1);
@@ -48,12 +73,11 @@ function moveForward () {
       document.getElementById("frame").style.marginLeft = (right) + "px";
     }
   };
-  var temp = slides.shift();
-  slides.push(temp);
-  document.getElementById('frame').innerHTML =  
-  '<div><img src="images/' + slides[2] + '"></div><div><img src="images/' + slides[1] + '"></div><div><img src="images/' + slides[0] + '"></div>';
-  console.log("Finish Forward " + slides[1]);
-  console.log(slides);
+  updateArrayForward ();
+  updateSlides();
+
+  //console.log("Finish Forward " + slides[1]);
+  //console.log(slides);
 };
 
 
