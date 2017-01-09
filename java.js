765aeda598d4f4770f9faa946b1c8022f0fd6a4b
Tim Erickson <tim@politalk.org>
2017-01-08 22:51:39 -0600

    Updating comments in JS code

--- java.js
+++ java.js
@@ -1,17 +1,24 @@
+// I plan to add keycode navigation of slideshow
+// Left Arrow = keycode 37
+// Right Arrow = keycode 39
+
 
 // The visible slide at start will be the slide
 // with the index of 1
 var activeSlide = 1;
 var captionVisible = true;
 
-displayCenterSlide();
-//startAutoForward();
 
+// Calls function for initial placement of slides
+displayCenterSlide();
+
+//Creating variables for event listeners. (This could be more effecient.)
 var clickNext = document.querySelector('.arrow-next');
 var clickPrev = document.querySelector('.arrow-prev');
 var clickAuto = document.querySelector('.auto-forward');
 var clickStop = document.querySelector('.stop-forward');
 
+// Adding listeners
 clickNext.addEventListener('click', moveNextCheck);
 clickNext.addEventListener('mouseover', hoverRight);
 clickNext.addEventListener('mouseout', offRight);
@@ -20,24 +27,27 @@ clickPrev.addEventListener('mouseout', offLeft);
 clickPrev.addEventListener('click', movePrevCheck);
 clickAuto.addEventListener('click', startAutoForward);
 clickStop.addEventListener('click', stopAutoForward);
+
+// Hiding inactive button until it's needed.
 clickStop.style.display = 'none';
 
+// Changing navigation images on hover for better UI
 function hoverRight() {
   document.querySelector('.arrow-next').src = 'images/arrow-next-hover.png';
 }
-
 function hoverLeft() {
   document.querySelector('.arrow-prev').src = 'images/arrow-prev-hover.png';
 }
-
 function offRight() {
   document.querySelector('.arrow-next').src = 'images/arrow-next.png';
 }
-
 function offLeft() {
   document.querySelector('.arrow-prev').src = 'images/arrow-prev.png';
 }
 
+// When we start auto play, we hide other navigation buttons
+// We also change border of slideshow to indicate it is in 
+// auto play mode.
 function startAutoForward() {
   quickStart = setInterval(moveNext, 20);
   setTimeout(pause, 21);
@@ -49,10 +59,8 @@ function startAutoForward() {
   document.getElementById('frame').style.borderColor = "maroon";
 }
 
-function pause() {
-  clearInterval(quickStart);
-}
-
+// When we stop auto play, we need to show the manual navigation buttons
+// We change the border of slideshow back to black.
 function stopAutoForward() {
   clearInterval(autoForward);
   clickAuto.style.display = 'inline';
@@ -62,6 +70,16 @@ function stopAutoForward() {
   document.getElementById('frame').style.borderColor = "black";
 }
 
+// When you start the autoplay, the first slide should move 
+// quicker than subsequent slides This function does that.
+function pause() {
+  clearInterval(quickStart);
+}
+
+
+// The slideshow keeps three slides in the DOM at all times. 
+// The active (center) slide and one to the left and one to the right
+// This fuctions makes sure that all three slides are up to date.
 function displayCenterSlide() {
   document.getElementById('slides').classList.add('animate');
 
@@ -76,20 +94,26 @@ function displayCenterSlide() {
   document.getElementById('active-slide').children[0].classList.add('up');
 }
 
+// These fuctions were supposed to turn off the auto play when
+// a user tries to manually navigate the slideshow. We
+// removed this option, by hiding manual navigation 
+// during auto play.
 function moveNextCheck() {
-  if (autoForward = false) {
-    clearInterval(autoForward);
-  }
+  // if (autoForward = false) {
+  //   clearInterval(autoForward);
+  // }
   moveNext();
 }
 
 function movePrevCheck() {
-  if (autoForward = false) {
-    clearInterval(autoForward);
-  }
+  // if (autoForward = false) {
+  //   clearInterval(autoForward);
+  // }
   movePrev();
 }
 
+
+// This controls all the animation when slides are moving forward
 function moveNext() {
   document.getElementById('slides').classList.remove('animate-none');
   document.getElementById('active-slide').children[0].classList.add('move-down');
@@ -99,7 +123,6 @@ function moveNext() {
   setTimeout(function(){ 
     updateArrayForward ();
     document.getElementById('active-slide').innerHTML = '<div class="caption down">' + slides[activeSlide]['caption'] + '</div><img src="images/' + slides[activeSlide]['fileName'] + '">';
-    //console.log(slides[activeSlide]['caption'] + ' and ' + slides[activeSlide]['fileName']);
   }, 2000);
   if (slides[activeSlide + 1]['caption'].length == 0) {
     captionVisible = false;
@@ -120,6 +143,7 @@ function moveNext() {
   }, 2800); 
 }
 
+// This controls all the animation when slides are moving backwards
 function movePrev() {
   document.getElementById('slides').classList.remove('animate-none');
   document.getElementById('active-slide').children[0].classList.add('move-down');
@@ -152,13 +176,13 @@ function movePrev() {
   }, 3800); 
 }
 
-/* Moves images in array backward One */
+// Moves images in array backward One 
 function updateArrayBack() {
   var temp = slides.pop();
   slides.unshift(temp);
 }
 
-/* Moves images in array forward One */
+// Moves images in array forward One 
 function updateArrayForward() {
   var temp = slides.shift();
   slides.push(temp);
